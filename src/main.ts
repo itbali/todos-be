8f0ff7747fe5c5312c0eb7d335ac796544b6d6ed
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-08-22 19:07:36 +0200

    vercel-build 14

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import {setupSwagger} from "./configSwagger";
 
 const createAndSetupApp = async () => {
@@ -17,17 +16,6 @@ const createAndSetupApp = async () => {
 
 async function bootstrap() {
   const app = await createAndSetupApp();
-
-
-  // Настройка Swagger
-  const config = new DocumentBuilder()
-      .setTitle('Todo API')
-      .setDescription('API for managing todos')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
