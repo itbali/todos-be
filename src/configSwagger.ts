b4a2be7d6cdc6803c8d39ef70f84834856472f0f
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-08-22 19:15:54 +0200

    vercel-build 16

--- src/configSwagger.ts
+++ src/configSwagger.ts
@@ -11,13 +11,13 @@ export const setupSwagger = (app: INestApplication) => {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('/api', app, document, {
         customJs: [
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.js',
         ],
         customCssUrl: [
-            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css',
         ],
-        customfavIcon: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/favicon-32x32.png',
+        // customfavIcon: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/favicon-32x32.png',
         customSiteTitle: 'Todo API Documentation',
     });
 };
