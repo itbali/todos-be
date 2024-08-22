1ebdc5bb707c078a4ca3702db9212a8669a4bde6
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-08-22 18:52:12 +0200

    vercel-build 11

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {setupSwagger} from "./configSwagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  setupSwagger(app);
   // Включение CORS для разработки
   app.enableCors({
     origin: '*',
