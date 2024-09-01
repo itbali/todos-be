37224d7b603dc882d82d8934b65db52687ba8452
Alex Duzhev <alexey.duzhev@tmrw.com>
2024-09-01 14:11:20 +0200

    opened cors 2

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,28 @@ const createAndSetupApp = async () => {
     setupSwagger(app);
 
     // Включение CORS для разработки
-    app.enableCors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        allowedHeaders: 'Content-Type, Accept, Authorization',
-        credentials: true,
+    app.enableCors((req, callback) => {
+        const corsOptions = {
+            origin: false,
+            methods: '',
+            allowedHeaders: '',
+            credentials: false,
+        };
+
+        const whitelist = ['http://localhost:3000', 'http://localhost:5173'];
+        const origin = req.header('Origin');
+
+        if (whitelist.includes(origin) || origin.includes('vercel.app')) {
+            corsOptions.origin = true;
+            corsOptions.methods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+            corsOptions.allowedHeaders = 'Content-Type, Accept, Authorization';
+            corsOptions.credentials = true;
+        }
+        callback(null, corsOptions);
     });
 
     return app;
-}
+};
 
 async function bootstrap() {
     const app = await createAndSetupApp();
